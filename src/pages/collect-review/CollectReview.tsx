9810e15d7ead29dca7bf756683fe4e31a1cd9083
its-rajesh-smp <its.rajeshsmp@gmail.com>
2024-10-19 18:47:39 +0530

    REFACTOR: Refactored the code

--- src/pages/collect-review/CollectReview.tsx
+++ src/pages/collect-review/CollectReview.tsx
@@ -1,12 +1,12 @@
 import { Card } from "@/common/components/shadcn/ui/card";
-import ThankYou from "./components/pages/ThankYou";
+import Form from "./components/pages/Form";
 
 function CollectReview() {
   return (
     <div className="lg:p-20 p-2  flex justify-center">
       <Card className="p-6  w-full lg:w-1/3">
-        {/* <Form /> */}
-        <ThankYou />
+        <Form />
+        {/* <ThankYou /> */}
       </Card>
     </div>
   );
